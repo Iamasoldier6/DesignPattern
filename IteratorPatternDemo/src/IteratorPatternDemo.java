5af97cf28a97179c7f704aec9981e4571262b3d1
Iamasoldier6 <iamasoldiersix@gmail.com>
2017-02-05 00:23:20 +0800

    迭代器模式

---
+++ IteratorPatternDemo/src/IteratorPatternDemo.java
@@ -0,0 +1,10 @@
+public class IteratorPatternDemo {
+	public static void main(String[] args) {
+		NameRepository namesRepository = new NameRepository();
+
+		for (Iterator iterator = namesRepository.getIterator(); iterator.hasNext();) {
+			String name = (String) iterator.next();
+			System.out.println("Name: " + name);
+		}
+	}
+}
